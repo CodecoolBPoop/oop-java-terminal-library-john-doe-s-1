6fe27e00a0c6ab66a97114b76218a23abbd6c4a0
adamszabolcs <szabolcs.adam91@gmail.com>
2018-09-04 11:19:27 +0200

    Tried setcolor method

--- src/com/codecool/termlib/Main.java
+++ src/com/codecool/termlib/Main.java
@@ -3,6 +3,7 @@ package com.codecool.termlib;
 class Main {
     public static void main(String[] args) {
         Terminal mainTerminal = new Terminal();
-        mainTerminal.clearScreen();
+        mainTerminal.setColor(Color.RED);
+        System.out.println("RED!");
     }
 }
